c89fb1a58bb3313fda1b99db1f6c5568f159a6ca
JyotiSuri <jyoti.suri@sjsu.edu>
2020-11-02 15:19:10 -0800

    React Login Code

---
+++ Login-Page/src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './login.jsx';
+import NavBar from './NavBar.jsx';
+import { PrivateRoute } from "./PrivateRoute.jsx";
+import {isLoggedIn} from './auth.js';
+import axios from 'axios'; 
+import logo from './summarAIze.png';
+import useSignUpForm from './CustomHooks';
+import './App.css';
+
+// const signup = () => {
+//   alert(`User Created!
+//          Name: ${inputs.firstName} ${inputs.lastName}
+//          Email: ${inputs.email}`);
+// }
+// const {inputs, handleInputChange, handleSubmit} = useSignUpForm(signup);
+
+const Home = ()=> <h3>Logged in as {localStorage.getItem("username")}</h3>
+
+function App() {
+  return (
+    <>
+
+    <div className="App">
+      <header className="App-header">
+        <img style={{ height: '60px'}} src={logo} className="App-logo" alt="logo" />
+        <Router>
+        <div>
+          <NavBar/>
+          <PrivateRoute exact isloggedin={isLoggedIn()} path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
+        &nbsp; &nbsp; &nbsp; Sign In or Sign Up with summarAIze to save your videos and documents
+        {/* <button type="password">Sign Up</button> */}
+        {/* <form>
+          <label>Username</label>
+          <input type="text"/>
+          <label> Password</label>
+          <input type="password"/>
+          <button type="password">Login</button>
+      </form> */}
+      </header>
+      
+      
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
+
